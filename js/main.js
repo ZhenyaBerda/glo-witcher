8a8950dfadd7a675ddf6b174e1ed2f559c8bb432
brdnkva <bev030997@gmail.com>
2020-10-23 10:51:01 +0300

    add responsive layout

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 const mySwiper = new Swiper('.swiper-container', {
-   slidesPerView: 2,
+   slidesPerView: 1,
     loop: true,
     navigation: {
        nextEl: '.swiper__arrow'
+    },
+    breakpoints: {
+       540: {
+           slidesPerView: 2,
+       }
     }
 
 });
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('header-active');
+    menuButton.classList.toggle('menu-button-active');
+})
